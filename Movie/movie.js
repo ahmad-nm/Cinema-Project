f7a671418598d9ccc8419095b5de48ddd0d0d1e0
Ahmad naim <ahmad.naim@st.ul.edu.lb>
2024-12-29 22:31:54 +0200

    fixed some issues

--- Movie/movie.js
+++ Movie/movie.js
@@ -57,20 +57,6 @@ fetch(
         document.getElementById("error-message").innerHTML = "Movie not found";
     });    
 
-    const navBar = document.querySelector("nav"),
-    menuBtns = document.querySelectorAll(".menu-icon"),
-    overlay = document.querySelector(".overlay");
-    
-    menuBtns.forEach((menuBtn) => {
-        menuBtn.addEventListener("click", () => {
-            navBar.classList.toggle("open");
-        });
-    });
-    
-    overlay.addEventListener("click", () => {
-        navBar.classList.remove("open");
-    });
-
     function changefavorite(){
         let fav = document.getElementById("favorite-icon").src;
         
